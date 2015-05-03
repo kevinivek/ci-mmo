703a406ba0b31e9423012d04378ae62ce6fd1ad7
Kevin Weeks <kevin.weeks@comcast.net>
2015-05-03 06:27:59 -0700

    Added in reading database connection credentials from the file "database_connection_config.txt" found in the "Assets/scripts" folder. WARNING!!: When modifying this config file, DO NOT ACCIDENTLY push that updated file back to the origin repo (github), otherwise you will be giving your server credentials to the entire world as the repo is public. So make sure that after you make the changes to the config file, keep it from entering your next commit, either unstage it or set it to be ignored.

--- Assets/scripts/test_database.cs
+++ Assets/scripts/test_database.cs
@@ -1,20 +1,47 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Data;
 using System.Data.SqlClient;
+using System.IO;
 
 
 public class test_database : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		string connString = "server=server;uid=user;pwd=pwd;";
+		string connString = getConnString ();
 		SqlConnection conn = new SqlConnection(connString);
 		conn.Open();
-		conn.Close();	
+		conn.Close();
+	}
+
+	public String getConnString() {
+
+		FileInfo sourceFile;
+		sourceFile = new FileInfo ("Assets/scripts/database_connection_config.txt");
+		StreamReader reader = sourceFile.OpenText ();
+
+		String connString = "";
+		String line = "";
+		while ((line = reader.ReadLine ()) != null) {
+			if(line.Length<2)
+				continue;
+			if (line[0] == '#') {
+				if(line[1] == '#') {
+					continue;
+				}
+				String name = line.Substring(1,line.Length-1);
+				String value = reader.ReadLine(); 
+				connString += name + "=" + value + ";";
+			}
+		}
+
+		reader.Close ();
+
+		return connString;
 	}
 
 }
