e0fbe0374b87628acfb15c4ca7739ed84893aabb
Kevin Weeks <kevin.weeks@comcast.net>
2015-05-03 05:27:26 -0700

    Added in basic database connection code

--- Assets/test_database.cs
+++ Assets/test_database.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
-public class NewBehaviourScript : MonoBehaviour {
+
+public class test_database : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		string connString = "server=server;uid=user;pwd=pwd;";
+		SqlConnection conn = new SqlConnection(connString);
+		conn.Open();
+		conn.Close();	
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
