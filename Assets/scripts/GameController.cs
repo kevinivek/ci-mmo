b501d838dcb7ddf0fc04cfa3691ef3aece42d7c2
Kevin Weeks <kevin.weeks@comcast.net>
2015-05-04 18:57:38 -0700

    Added UI menu for when the game is first launched, can input id and hit submit to initiate the player.

--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,16 +10,28 @@ using System.IO;
 using MySql.Data.MySqlClient;
 
 
-public class TestDatabase : MonoBehaviour {
+public class GameController : MonoBehaviour {
+
+	public GameObject loginWindow;
+	public InputField userID;
+	public GameObject playerPrefab;
+	public GameObject player;
 
 	DatabaseConnection db;
 
 	// Use this for initialization
 	void Start () {
+		loginWindow.SetActive (true); 
+	}
+
+	public void initPlayer() {
+		loginWindow.SetActive (false);
+		player = Instantiate (playerPrefab);
+	}
+	
+	public void testConnect() {
 		db = new DatabaseConnection ();
-
 		int value = db.getValue<int> ("id", Convert.ToString(1), "height");
-
 		transform.Translate (Vector2.up * value);
 	}
 
